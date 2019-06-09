25ad437e38c1e233b54f490b1eab9525e0aa1b19
salviolorenzo <salviolorenzo@gmail.com>
2019-06-09 16:54:00 -0400

    adds welcome page with buttons

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './assets/components/header';
+import Welcome from './assets/components/welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Welcome />
     </View>
   );
 }
--- frontend/assets/components/welcome.js
+++ frontend/assets/components/welcome.js
@@ -1,7 +1,79 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native-elements';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { Image, Button } from 'react-native-elements';
+import logo from '../images/noname.png';
+const console = require('console');
+
+const onPress = event => {
+  alert(event.target.value);
+};
 
 export default function welcome() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../images/noname.png')}
+        style={{ width: 200, height: 200 }}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+      <Text style={styles.welcomeText}>Welcome to LifeLine.</Text>
+      <Text style={styles.subText}>
+        The social app centered around connection and wellbeing
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={event => {
+            onPress(event);
+          }}
+          buttonStyle={styles.buttons}
+          type="solid"
+          title="Log In"
+          raised={true}
+        />
+        <Button
+          onPress={event => {
+            onPress(event);
+          }}
+          buttonStyle={styles.buttons}
+          type="solid"
+          title="Register"
+          raised={true}
+        />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+
+  img: {},
+  welcomeText: {
+    margin: 5,
+    fontSize: 36,
+    color: '#25ceff'
+  },
+  subText: {
+    margin: 5,
+    fontSize: 12
+  },
+
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start'
+  },
+
+  buttons: {
+    backgroundColor: '#25ceff',
+    borderRadius: 10,
+    width: 100,
+    height: 50
+  }
+});
